2c35c8666b902154fa196a28db64b1d0b6889fb4
Siôn le Roux <sinisterstuf@gmail.com>
2012-05-25 16:05:42 +0200

    extended test and created fixture

--- mysite/fts/tests.py
+++ mysite/fts/tests.py
@@ -1,7 +1,9 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class PollsTest(LiveServerTestCase):
+	fixtures = ['admin_user.json']
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -16,6 +18,23 @@ class PollsTest(LiveServerTestCase):
 		# She sees the familiar 'Django administration' heading
 		body = self.browser.find_element_by_tag_name('body')
 		self.assertIn('Django administration', body.text)
+
+		# She types in her username and password and hits return
+		username_field = self.browser.find_element_by_name('username')
+		username_field.send_keys('admin')
+
+		password_field = self.browser.find_element_by_name('password')
+		password_field.send_keys('adm1n')
+		password_field.send_keys(Keys.RETURN)
+
+		# her username and password are accepted and she is taken to
+		# the Site Administration page
+		body = self.browser.find_element_by_tag_name('body')
+		self.assertIn('Site administration', body.text)
+
+		# She now sees a couple of hyperlinks that say "Polls"
+		polls_links = self.browser.find_elements_by_link_text('Polls')
+		self.assertEquals(len(polls_links), 2)
 
 		# TODO: use the admin site to create a Poll
 		self.fail('finish this test')
